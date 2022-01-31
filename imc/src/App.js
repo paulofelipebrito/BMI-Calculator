9646ad43665ac161492c5594762baadee2b489cb
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-01-31 17:31:09 -0300

    Calculating index and showing message.

--- imc/src/App.js
+++ imc/src/App.js
@@ -4,9 +4,21 @@ import { useState } from 'react';
 function App() {
   const [weight, setWeight] = useState('');
   const [height, setHeight] = useState('');
+  const [message, setMessage] = useState('');
 
   function handleClick(){
-    alert("clicou")
+    const heightFormula = height/100;
+    const imc = weight / ((height/100) * (height/100))
+
+    if(imc < 18.6){
+      setMessage("Você está abaixo do peso! Seu IMC = " + imc.toFixed(2))
+    } else if(imc >= 18.6 && imc < 24.9){
+      setMessage("Peso Ideal! Seu IMC = " + imc.toFixed(2))
+    } else if(imc >= 24.8 && imc < 34.9){
+      setMessage("Você está levemente acima do peso! Seu IMC = " + imc.toFixed(2))
+    }else if(imc >= 34.9){
+      setMessage("Cuidado, obesidade! Seu IMC = " + imc.toFixed(2))
+    }
   }
 
   return (
@@ -36,7 +48,7 @@ function App() {
 
       </div>
 
-      <h2>Seu IMC foi 25, você está acima do peso.</h2>
+      <h2>{message}</h2>
 
     </div>
   );
