1cfc61159cc4fd45e3b5e2167525805a842d3def
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-08-05 09:50:05 -0300

    feat: changing the language to english

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ function App() {
 
   function handleClick(){
     const heightFormula = height/100;
-    const imc = weight / ((height/100) * (height/100))
+    const imc = weight / (heightFormula * heightFormula)
 
     if(imc < 18.6){
-      setMessage("Você está abaixo do peso! Seu IMC = " + imc.toFixed(2))
+      setMessage("You are underweight! Your BMI = " + imc.toFixed(2))
     } else if(imc >= 18.6 && imc < 24.9){
-      setMessage("Peso Ideal! Seu IMC = " + imc.toFixed(2))
+      setMessage("Ideal weight! Your BMI = " + imc.toFixed(2))
     } else if(imc >= 24.8 && imc < 34.9){
-      setMessage("Você está levemente acima do peso! Seu IMC = " + imc.toFixed(2))
+      setMessage("You are slightly overweight! Your BMI = " + imc.toFixed(2))
     }else if(imc >= 34.9){
-      setMessage("Cuidado, obesidade! Seu IMC = " + imc.toFixed(2))
+      setMessage("Be careful, obesity! Your BMI = " + imc.toFixed(2))
     }
   }
 
